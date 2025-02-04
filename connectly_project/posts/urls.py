e7ed075c5bec5329fd373855ebd367892972a6b3
amcastelo <162676224+amcastelo@users.noreply.github.com>
2025-02-04 14:31:41 +0800

    Added Auth_Token_Getter

--- connectly_project/posts/urls.py
+++ connectly_project/posts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import UserListCreate, PostListCreate, CommentListCreate
+from .views import UserListCreate, PostListCreate, CommentListCreate, ProtectedView
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('users/', UserListCreate.as_view(), name='user-list-create'),
     path('posts/', PostListCreate.as_view(), name='post-list-create'),
     path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
+    path('pv/', ProtectedView.as_view(), name='protected-view'),
+    path('token-auth/', obtain_auth_token, name='obtain-token-auth'),
 ]
 
